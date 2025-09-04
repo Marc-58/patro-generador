d30440c78cfd9d1598231f180ce5c230176286b3
Marc-58 <mqueralt16@gmail.com>
2025-09-04 20:15:14 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -378,8 +378,15 @@ p.line(
     }if (tipus === "faldillaMitjaCapa") {
       const cintura = mides.cintura * escala;
       const llarg = mides.llarg * escala;
+        const radi = cintura / 2 * Math.PI; 
 
-      p.line(marge, marge, marge, marge + llarg);
+    // Dibuixar arcs
+    p.arc(marge + llarg + radi, marge, llarg + radi, llarg + radi,  p.HALF_PI, p.PI); 
+    p.arc(marge + llarg + radi, marge, radi, radi,  p.HALF_PI, p.PI); 
+
+    // Línia esquerra
+    p.line(marge, marge, marge + llarg + radi / 2, marge);
+  
      
     } if (tipus === "faldillaCapaSencera") {
       const cintura = mides.cintura * escala;
@@ -393,8 +400,6 @@ p.line(
     // Arc de cintura (semicercle cap avall)
     p.arc(marge + radi+llarg , marge , 2 *radi ,2*radi, 0, p.PI);
     p.arc(marge + radi+llarg , marge , 2 * radi+llarg*2, 2 * radi+llarg*2, 0, p.PI);
-
-
     // Línia inferior
     p.line(marge, marge, marge + 2 * radi*2+llarg*2, marge );
 }
