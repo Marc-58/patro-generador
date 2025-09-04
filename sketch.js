4fb3704ecdc099b4ed725708942c5500a2fbdc72
Marc-58 <mqueralt16@gmail.com>
2025-09-04 19:27:36 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -293,8 +293,11 @@ function dibuixaPatro(p) {
     } if (tipus === "faldillaCapaSencera") {
       const cintura = mides.cintura * escala;
       const llarg = mides.llarg * escala;
+      const radi =   cintura/2*3.14;
 
-      p.line(marge, marge, marge, marge + llarg);
+    p.arc(marge + radi+llarg, marge , radi+llarg, radi+llarg, 0, p.PI);
+    p.arc(marge + radi+llarg, marge , radi, radi, 0, p.PI);
+    p.line(marge, marge, marge +2*llarg+radi*2, marge); // línia esquerra
      
     } 
   };
