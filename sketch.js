a3b5b61c52c7660098cb8beda03565bd0a1a16f4
Marc-58 <mqueralt16@gmail.com>
2025-09-05 16:32:47 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -364,8 +364,8 @@ p.line(
       
       p.rect(marge, marge, coll/2, 6*escala);
       
-      p.line(marge+coll/2-1,marge, marge+coll/6, marge+5*escala );
-      p.line(marge+coll/6,marge+6*escala, marge+coll/6, marge+5*escala );
+      p.line(marge+coll/2-1,marge, marge+coll/2, marge+5*escala );
+      p.line(marge+coll/6,marge+6*escala, marge+coll/2, marge+5*escala );
 
       
     }else if (tipus === "maniga") {
