765a59c8252ebbe8ff4d28751135bfbe3ef1e6ae
Marc-58 <mqueralt16@gmail.com>
2025-09-08 10:48:06 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -384,7 +384,7 @@ p.stroke("red");
 p.strokeWeight(6);
    
 
-       p.point(marge+contornBraç/4),marge+(llargTotal+llargBraç)/2);
+       p.point(marge+contornBraç/4,marge+(llargTotal+llargBraç)/2);
       p.point( marge+contornBraç*3/4,marge+(llargTotal+llargBraç)/2);
       p.point(marge+contornBraç/2, marge-2*escala);
 
