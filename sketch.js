53307429402b92363327c8e993b4078ae64383fd
Marc-58 <mqueralt16@gmail.com>
2025-10-02 22:49:48 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -637,7 +637,7 @@ p.beginShape();
 p.noFill();
 p.curveVertex(m2+cadera/4+cadera/20 , marge+20*escala+cadera/20); 
 p.curveVertex(m2+cadera/4+cadera/20 , marge+20*escala+cadera/20); 
-p.curveVertex(m2 + cadera/4+205*escala, marge+llargGenoll);
+p.curveVertex(m2 + cadera/4+2.5*escala, marge+llargGenoll);
 p.curveVertex(m2 + cadera/4+1.5*escala, marge+llarg);
 p.curveVertex(m2 + cadera/4+1.5*escala, marge+llarg); 
 p.endShape();
@@ -649,10 +649,33 @@ p.endShape();
       p.line(m2+cadera/4-(cintura/4+3)+cintura/8,marge, m2+cadera/4-(cintura/4+3)+cintura/8+3*escala ,marge+11*escala ); // pinsa
       p.line(m2+cadera/4-(cintura/4+3)+cintura/8+3*escala,marge, m2+cadera/4-(cintura/4+3)+cintura/8+3 *escala,marge+11*escala); // pinsa
 
+
+      // punts inicial i final
+let x1 = m2 + cadera/4;
+let y1 = marge + 20*escala;
+let x2 = m2 + cadera/4 + cadera/20;
+let y2 = marge + 20*escala + cadera/20;
+
+// punts de control (més cap a l’esquerra)
+let cx1 = x1 - cadera/10;
+let cy1 = y1 + cadera/40;
+let cx2 = x2 - cadera/10;
+let cy2 = y2 - cadera/40;
+
+// dibuixar la corba
+p.bezier(x1, y1, cx1, cy1, cx2, cy2, x2, y2);
+p.bezier(
+  marge + cadera/4 + cadera/10 + 1*escala, marge + tiro + 1*escala,   // punt inicial
+  marge + cadera/4 + cadera/10 + 1*escala - cadera/8, marge + tiro + 1*escala - tiro/10, // control 1
+  m2 + cadera/4 - cadera/8, marge + tiro - 13*escala + tiro/10,       // control 2
+  m2 + cadera/4, marge + tiro - 13*escala                             // punt final
+);
+
+      
       
 p.stroke("red");
 p.strokeWeight(6)
-       p.line(marge+cadera/4-(cintura/4+3+3),marge, marge+cadera/4-3*escala ,marge); // cint-cadera
+       p.line(marge+cadera/4-(cintura/4+3+3),marge, marge+cadera/4-3*escala ,marge); // cint
       p.line( marge+3*escala,marge+llarg, marge+cadera/4+2*escala ,marge+llarg); // baix
 
       
