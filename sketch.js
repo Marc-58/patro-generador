652fcdea3860299abb91211f9e9ecb938293d756
Marc-58 <mqueralt16@gmail.com>
2025-12-05 13:04:13 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -800,7 +800,7 @@ p.endShape();
     // Arc de cintura (semicercle cap avall)
     p.arc(marge + radi+llarg , marge , 2 *radi ,2*radi, 0, p.PI);
     p.arc(marge + radi+llarg , marge , 2 * radi+llarg*2, 2 * radi+llarg*2, 0, p.PI);
-    p.line(marge, marge, marge + 2 * radi*2+llarg*2, marge );
+    p.line(marge, marge, marge +radi*2+llarg*2, marge );
 }
   };
 }
