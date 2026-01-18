40167b2ae0f95c5ce11bfb14e2f92bcfae04f479
Marc-58 <mqueralt16@gmail.com>
2026-01-18 20:26:43 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -168,7 +168,7 @@ function generarPatro(peca) {
 
 function dibuixaPatro(p) {
   p.setup = function () {
-    const escala = 28.3464567;
+    const escala = 0.026458333333719;
     const marge = 50;
 
     if (tipus === "faldilla") {
