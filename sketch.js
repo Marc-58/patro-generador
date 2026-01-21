483b83102e2d9a17aedb968a22dc65b2c8e18ac6
Marc-58 <mqueralt16@gmail.com>
2026-01-21 13:14:22 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -225,7 +225,7 @@ function dibuixaPatro(p) {
       const marge = espatlles / 2 + 20 * escala + 10;
 
 
-      p.createCanvas(ciintura/2 + 20, talleDavanter +20 );
+      p.createCanvas(cintura/2 + 20, talleDavanter +20 );
       p.background(255);
       p.stroke(0);
       p.noFill();
